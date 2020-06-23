4b90ce493ba96b5f3e66c9a0f543d945dab03872
Anna Lizon <annakatarina1994@gmail.com>
2020-06-23 18:36:20 -0400

    Author POJO basic setup

--- src/main/java/com/practice/demo/Author.java
+++ src/main/java/com/practice/demo/Author.java
@@ -1,4 +1,58 @@
 package com.practice.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Set;
+
+@Entity
 public class Author {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Set<Book> books;
+
+    public Author(){}
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
--- src/main/java/com/practice/demo/Book.java
+++ src/main/java/com/practice/demo/Book.java
@@ -1,4 +1,10 @@
 package com.practice.demo;
 
+import java.util.Set;
+
 public class Book {
+    private Long id;
+    private String title;
+    private String isbn;
+    private Set<Author> authors;
 }
