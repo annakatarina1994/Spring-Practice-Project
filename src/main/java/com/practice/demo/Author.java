1a899cc91118b603560f57d8e4f66972ae5d03d4
Anna Lizon <annakatarina1994@gmail.com>
2020-06-23 18:38:06 -0400

    Book POJO basic setup

--- src/main/java/com/practice/demo/Author.java
+++ src/main/java/com/practice/demo/Author.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 @Entity
 public class Author {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
--- src/main/java/com/practice/demo/Book.java
+++ src/main/java/com/practice/demo/Book.java
@@ -1,10 +1,57 @@
 package com.practice.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
 
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String isbn;
     private Set<Author> authors;
+
+    public Book(){}
+
+    public Book(String title, String isbn, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 }
